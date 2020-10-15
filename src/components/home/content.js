53901508594b7224e67cd05a6a156d1e47a55850
cvimard <cvimard@socialobjects.co>
2020-10-15 14:55:05 +0200

    chore(my-react2): initialize project

---
+++ src/components/home/content.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Counter from '../counter';
+
+const Content = () => (
+  <main>
+    <Counter />
+  </main>
+);
+
+export default Content;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+
+const App = () => <div></div>;
+
+ReactDOM.render(<App />, document.getElementById('app'));
