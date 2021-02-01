bb44e3a4166db4272993f52f3b41980dd4969309
cvimard <cvimard@socialobjects.co>
2021-02-01 10:39:43 +0100

    core(package): reset project

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Home from './components/home';
-
 import './index.scss';
-
-const datas = [{
-  id: '12345',
-  name: 'Cyril',
-  phone: '0606060606',
-}, {
-  id: '1234',
-  name: 'Matt',
-  phone: '0707070707',
-}, {
-  id: '123',
-  name: 'David',
-  phone: '0808080808',
-}];
 
 const App = () => (
   <div>
-    <ul>
-      {datas.map((data) => <Home data={data} />)}
-    </ul>
+    <h1>Hello Wolrd</h1>
   </div>
 );
 
